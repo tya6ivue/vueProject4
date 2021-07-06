86669fdced382ffeedb46c2b1cd168b60c5aa1c4
tya6ivue <nishant_tyagi@excellencetechnologies.in>
2021-07-06 16:50:51 +0530

    first push

---
+++ App.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [
+      { text: "Learn Javascript" },
+      { text: "Learn Vue" },
+      { text: "Learn HTML" },
+      { text: "Learn CSS" },
+      { text: "Learn next Js" },
+      { text: "Build something awesome" },
+    ],
+  },
+});
